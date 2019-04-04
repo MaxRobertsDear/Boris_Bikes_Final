2ffe9e7485c5a926865ac3f2e5556b0a5693b69e
Calum Darroch <calum_darroch@hotmail.com>
2019-04-04 17:53:36 +0100

    Progress on show bike test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
+
+attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+    @bike = bike
+  end
+
+  def show_bike
+    
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,24 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
+
   it "release a working Bike" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it "stores docked bike" do
+    bike = subject.release_bike
+    expect(subject.dock_bike(bike)).not_to be_nil
+  end
+
+  it { is_expected.to respond_to(:show_bike) }
+
+  it "shows stored bike" do
+    bike = subject.dock_bike(bike)
+    expect(subject.show_bike).to eq bike
+  end
+
 end
