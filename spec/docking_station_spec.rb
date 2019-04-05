b87f9ce99c949f6f1638904d566c5a7e5e81706b
Calum Darroch <calum_darroch@hotmail.com>
2019-04-05 15:34:12 +0100

    Exception test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,17 +3,25 @@ require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
 
-  it "release a working Bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe "#release_bike" do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it "returns an error when no bikes available" do
+      expect { subject.release_bike }.to raise_error("No bike at docking station")
+    end
+
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it "stores docked bike" do
-    bike = subject.release_bike
-    expect(subject.dock(bike)).not_to be_nil
-  end
+  # it "stores docked bike" do
+  #   bike = subject.release_bike
+  #   expect(subject.dock(bike)).not_to be_nil
+  # end
 
   it "shows stored bike" do
     expect(subject.bike).to eq @bike
